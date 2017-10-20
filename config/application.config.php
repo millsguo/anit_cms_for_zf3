d24f38e2d6599ec797bd645e85af12334e1338d4
Romuald <rongeb@yahoo.ca>
2017-10-20 19:21:06 +0200

    [technical, minor bugfix] disable global cache configuration for dev and remove css reference for datetime picker

--- config/application.config.php
+++ config/application.config.php
@@ -51,21 +51,14 @@ return array(
             './module',
             './vendor',
         ),
-    // An array of paths from which to glob configuration files after
-    // modules are loaded. These effectively overide configuration
-    // provided by modules themselves. Paths may use GLOB_BRACE notation.
-    /*
-      'config_glob_paths' => array(
-      'config/autoload/{,*.}{global,local}.php',
-      ),
-     */
+
     // Whether or not to enable a configuration cache.
         // If enabled, the merged configuration will be cached and used in
         // subsequent requests.
-        'config_cache_enabled' => true,
+        //'config_cache_enabled' => true,
         // The key used to create the configuration cache file name.
         
-        'config_cache_key' => "124567890AZERTYUIOPQSDFGHJKLMWXC",
+        //'config_cache_key' => "124567890AZERTYUIOPQSDFGHJKLMWXC",
  
         // Whether or not to enable a module class map cache.
         // If enabled, creates a module class map cache which will be used
@@ -78,7 +71,7 @@ return array(
          
          
         // The path in which to cache merged configuration.
-        'cache_dir' => "./data/cache/",
+        //'cache_dir' => "./data/cache/",
     // Whether or not to enable modules dependency checking.
     // Enabled by default, prevents usage of modules that depend on other modules
     // that weren't loaded.
