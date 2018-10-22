97bdd6e4a3423ba63cbeae33536fde19e0db041e
rongeb <rongeb@yahoo.ca>
2018-10-22 12:21:04 +0200

    [bugfix] resolving merge conflicts

--- module/Pagews/src/Controller/PagewsController.php
+++ module/Pagews/src/Controller/PagewsController.php
@@ -8,17 +8,9 @@ use Rubrique\Model\RubriqueDao;
 use Sousrubrique\Model\Sousrubriquedao;
 use Pagearrangement\Model\PagearrangementDao;
 
-<<<<<<< HEAD
 class PagewsController extends AbstractActionController {
 
     public function getallpagesAction() {
-=======
-class PagewsController extends AbstractActionController
-{
-
-    public function getallpagesAction()
-    {
->>>>>>> admin_publishing
         $rubriqueDao = new RubriqueDao();
         $allPagesBySpace = $rubriqueDao->getAllRubriques("array");
 
@@ -27,12 +19,7 @@ class PagewsController extends AbstractActionController
         ));
     }
 
-<<<<<<< HEAD
     public function getallpagesbyspaceidAction() {
-=======
-    public function getallpagesbyspaceidAction()
-    {
->>>>>>> admin_publishing
         $spaceId = $this->params()->fromRoute('id');
         $rubriqueDao = new RubriqueDao();
         $allPagesBySpace = $rubriqueDao->getAllRubriquesBySpaceId($spaceId, "array");
@@ -41,8 +28,7 @@ class PagewsController extends AbstractActionController
             'data' => $allPagesBySpace
         ));
     }
-
-<<<<<<< HEAD
+/*
     public function getallsectionsbypageidAction() {
         $pageId = $this->params()->fromRoute('id');
         $sousrubriqueDao = new Sousrubriquedao();
@@ -51,43 +37,26 @@ class PagewsController extends AbstractActionController
             'data' => $sousrubriques
         ));
     }
-
+*/
     public function getpagearrangementbypagenameAction() {
-=======
-    /*
-        public function getallsectionsbypageidAction() {
-            $pageId = $this->params()->fromRoute('id');
-            $sousrubriqueDao = new Sousrubriquedao();
-            $sousrubriques = $sousrubriqueDao->getSousrubriquesByRubrique($pageId, "array");
-            return new JsonModel(array(
-                'data' => $sousrubriques
-            ));
-        }
-    */
-    public function getpagearrangementbypagenameAction()
-    {
->>>>>>> admin_publishing
         $filename = $this->params()->fromRoute('id');
-        // var_dump($filename);
         $rubriqueDao = new RubriqueDao();
         $rubrique = $rubriqueDao->getRubriqueByFilename($filename, "array");
-        $rubriqueId = (int)$rubrique['id'];
-        // var_dump($rubriqueId);
-        // exit;
+        $rubriqueId = (int) $rubrique['id'];
+        //var_dump($rubriqueId);
         $pageArrangementDao = new PagearrangementDao();
         $pageArrangement = $pageArrangementDao->getPage($rubriqueId, "asc", "array");
-
+        
         return new JsonModel(array(
             'data' => $pageArrangement
         ));
     }
 
-    public function getpagearrangementbypageidAction()
-    {
+    public function getpagearrangementbypageidAction() {
         $id = (int)$this->params()->fromRoute('id');
         $pageArrangementDao = new PagearrangementDao();
         $pageArrangement = $pageArrangementDao->getPage($id, "asc", "array");
-
+        
         return new JsonModel(array(
             'data' => $pageArrangement
         ));
